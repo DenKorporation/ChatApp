dd1241befb6748aa441f9c97c02664637d0b77be
_DEN_ <kononchukd04@gmail.com>
2023-03-18 22:47:15 +0300

    client project added

---
+++ Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+Console.WriteLine("Enter your message:");
+var outputMessage = Console.ReadLine();
+
+IPEndPoint server;
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+bool isConnected = false;
+
+while (!isConnected)
+{
+    Console.WriteLine("Choose EndPoint: ");
+    while (!IPEndPoint.TryParse(Console.ReadLine(), out server))
+    {
+        Console.WriteLine("Try again");
+    }
+
+    Console.WriteLine($"server: {server}. Trying connect...");
+    try
+    {
+        socket.Connect(server);
+        Console.WriteLine("Connection successfully");
+        isConnected = true;
+    }
+    catch (SocketException)
+    {
+        Console.WriteLine("Connect failed. Try again?(y/other)");
+        if (Console.ReadLine() != "y")
+        {
+            return;
+        }
+    }
+}
+
+socket.Send(Encoding.UTF8.GetBytes(outputMessage));
+
+StringBuilder recieveMessage = new StringBuilder();
+int bytesRead = 0;
+byte[] buffer = new byte[256];
+    
+do
+{
+    bytesRead = socket.Receive(buffer);
+    string text = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    recieveMessage.Append(text);
+} while (bytesRead > 0);
+
+Console.WriteLine($"Reply: {recieveMessage}");
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+
+
+IPEndPoint server;
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+bool isConnected = false;
+
+while (!isConnected)
+{
+    Console.WriteLine("Choose EndPoint: ");
+    while (!IPEndPoint.TryParse(Console.ReadLine(), out server))
+    {
+        Console.WriteLine("Try again");
+    }
+
+    Console.WriteLine($"server: {server}. Trying start listening...");
+    try
+    {
+        socket.Bind(server);
+        socket.Listen();
+        Console.WriteLine("Listening started");
+        isConnected = true;
+    }
+    catch (SocketException)
+    {
+        Console.WriteLine("start failed. Try again?(y/other)");
+        if (Console.ReadLine() != "y")
+        {
+            return;
+        }
+    }
+}
+
+while (true)
+{
+    using Socket clientSocket = socket.Accept();
+    Console.WriteLine("Connection successfully");
+    
+    StringBuilder recieveMessage = new StringBuilder();
+    int bytesRead = 0;
+    byte[] buffer = new byte[256];
+    
+    do
+    {
+        bytesRead = clientSocket.Receive(buffer);
+        string text = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        recieveMessage.Append(text);
+    } while (bytesRead > 0);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {recieveMessage}");
+    
+    byte[] outputMessage = Encoding.UTF8.GetBytes("message received");
+    clientSocket.Send(outputMessage);
+        
+    clientSocket.Shutdown(SocketShutdown.Both);
+}
